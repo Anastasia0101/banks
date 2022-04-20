89a9a8acdcfacbd77c80326c7353707933d92399
Anastasia <anastasia.storozhenko@masterofcode.com>
2022-04-21 00:19:59 +0300

    connect firebase

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyCW48ZqWXhYStcfUIosbS6nY6vIpQ2zC6Y",
+    authDomain: "banks-app-f8fff.firebaseapp.com",
+    projectId: "banks-app-f8fff",
+    storageBucket: "banks-app-f8fff.appspot.com",
+    messagingSenderId: "811736842637",
+    appId: "1:811736842637:web:e95d52933fccf1abcc3dc4",
+    measurementId: "G-LJ4ZZYCPRB"
+  }
 };
 
 /*
